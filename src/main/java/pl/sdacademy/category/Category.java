ace3434bb291e5f3e7d8aa3c087d3ec8f553a165
moja nazwa <jakub.bylak@gmail.com>
2021-08-19 15:10:19 +0200

    added category adding method

--- src/main/java/pl/sdacademy/category/Category.java
+++ src/main/java/pl/sdacademy/category/Category.java
@@ -27,4 +27,12 @@ public class Category {
     public String getName() {
         return name;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/pl/sdacademy/category/CategoryController.java
+++ src/main/java/pl/sdacademy/category/CategoryController.java
@@ -3,6 +3,8 @@ package pl.sdacademy.category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +23,16 @@ public class CategoryController {
         List<Category> categories = categoryRepository.findAll();
         modelMap.addAttribute("categories", categories);
         return "categories";
+    }
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute ("category") Category category){
+        return "category/form";
+    }
+
+    @PostMapping("/add")
+    public String addCategory(Category category){
+        categoryRepository.save(category);
+        return "redirect:/category";
     }
 }
