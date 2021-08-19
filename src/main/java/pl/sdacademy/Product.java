35940070f3cf8d2b931ee47913972bb031c8e159
WojtekTomaszCzubak <wojtek.czubakk@gmail.com>
2021-08-19 15:16:53 +0200

    Added Product class. Added ProductType enumeric class. Added ProductRepositoryJPA interface which extends JpaRepository<Product, Integer> interface.

--- src/main/java/pl/sdacademy/Product.java
+++ src/main/java/pl/sdacademy/Product.java
@@ -1,5 +1,6 @@
 package pl.sdacademy;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +11,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String title;
+    private String name;
     private String description;
     private String url;
     private String category;
@@ -20,10 +21,34 @@ public class Product {
     }
 
     public Product(String title, String description, String url, String category, double price) {
-        this.title = title;
+        this.name = title;
         this.description = description;
         this.url = url;
         this.category = category;
         this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
