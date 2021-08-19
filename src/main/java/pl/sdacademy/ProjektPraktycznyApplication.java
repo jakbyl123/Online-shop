8756db293b8b8935d45fdf41cef076f7b03ddf89
moja nazwa <jakub.bylak@gmail.com>
2021-08-19 11:59:56 +0200

    first commit

---
+++ src/main/java/pl/sdacademy/ProjektPraktycznyApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPraktycznyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektPraktycznyApplication.class, args);
+    }
+
+}
