05ef867853785aa8dc9ba43721e8e94af3af893d
moja nazwa <jakub.bylak@gmail.com>
2021-08-24 15:32:29 +0200

    added admin panel

--- src/main/java/pl/sdacademy/controllers/LoggedInController.java
+++ src/main/java/pl/sdacademy/controllers/LoggedInController.java
@@ -3,12 +3,18 @@ package pl.sdacademy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoggedInController {
     public final static String PATH = "/logged-in";
 
     @GetMapping(PATH)
-    public String getLoggedInPage() {
+    public String getLoggedInPage(HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "login/admin-panel";
+        }
+
         return "login/logged-in";
     }
 
