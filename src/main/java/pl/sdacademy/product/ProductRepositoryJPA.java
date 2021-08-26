c3b59ccdad514fa99098ce74c8c89035888db356
moja nazwa <jakub.bylak@gmail.com>
2021-08-26 12:12:18 +0200

    added search button corrected

--- src/main/java/pl/sdacademy/product/ProductRepositoryJPA.java
+++ src/main/java/pl/sdacademy/product/ProductRepositoryJPA.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface ProductRepositoryJPA extends JpaRepository<Product, Integer> {
-    @Query("SELECT p FROM Product p WHERE CONCAT(COALESCE(p.name, ''), COALESCE(p.category, ''), COALESCE(p.description, ''), COALESCE(p.price, 0)) LIKE %:param%")
+    @Query("SELECT p FROM Product p WHERE CONCAT(COALESCE(p.name, ''), COALESCE(p.category.name, ''), COALESCE(p.description, ''), COALESCE(p.price, 0)) LIKE %:param%")
     List<Product> search(@Param("param") String keyword);
 }
